b61fb6be5bd473b15f1ba3483f770f2d9d4f6bad
bakaVaka <baka@vaka,com>
2023-05-20 09:12:55 +0300

    [+] Починен фронтенд [+] Заготовка под сервис для хранения файликов

---
+++ src/clients/spa-client/src/components/b-input/b-input.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+import { defineProps, defineEmits } from 'vue';
+
+type inputType = "text" | "email" | "password"
+
+defineProps({
+    label:String,
+    value:String,
+    type:
+});
+const emit = defineEmits(['update:value'])
+
+function onValueChanged(e:Event){
+    emit('update:value', e.target.value)
+}
+
+</script>
+
+<template>
+    <div class="input-wrapper">
+        <label>{{ label }}</label>
+        <input :value="value" @input="onValueChanged"/>
+    </div>
+</template>
+
+<style scoped>
+input{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+.input-wrapper{
+    display: flex;
+    flex-direction: column;
+    align-items: baseline;
+    width: 100%;
+    position: relative;
+}
+label,input{ width: inherit; }
+
+label{ 
+    font-size: 0.8rem; 
+    margin-left: 10px;
+    color: #aaa;
+}
+
+input{ 
+    padding: 4px;
+}
+
+</style>
